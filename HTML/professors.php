30e9c09121b1751454e88f7fff27beed33e7115d
BryceRinderknecht <103059795+BryceRinderknecht@users.noreply.github.com>
2023-08-25 18:08:32 -0500

    Add files via upload

---
+++ HTML/professors.php
@@ -0,0 +1,200 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <title>NHMS Professors</title>
+	
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+  <link href="../css/styles.css" rel="stylesheet" type="text/css">	
+  <link href="../css/menu.css" rel="stylesheet" type="text/css">
+  <link href="../css/stylesProfessors.css" rel="stylesheet" type="text/css">	
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+	
+<body>
+	<div id="main">
+		<header>
+			<button class="menu"><span onclick="openNav()">Menu</span></button>
+			<a href="index.html">
+				<img src="../images/MNU_Header_Logo.svg" alt="MNU Header" height="80px">
+				<div class="headerSpacer"> </div>
+			</a>
+		</header>
+
+		<div id="mySidenav" class="sidenav">
+  			<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+			<a href="index.html">Home</a>
+			<a href="https://www.mnu.edu/undergraduate-admissions/">Admissions</a>
+			<a href="advising.html">Advising</a>
+			<a href="droneTour.html">Drone Tour</a>
+			<a href="professors.php">Professors</a>
+		</div>
+
+		<h1>Learn more about our professors in the department!</h1>
+		
+		<button type="button" class="profButton">Dr. Mark Brown</button>
+		<div class="profContent">
+			<p>Email: mabrown@mnu.edu</p>
+			<p>Office Number: 913-971-3663</p>
+			<p>Office: OS-228</p>
+			<p>Education: (PhD) 2012, University of Missouri-Kansas City, (MS) 1993, Wichita State University, (BA) 1991, MidAmerica Nazarene University</p>
+		</div>
+		
+		<button type="button" class="profButton">Dr. Chelsea Comadoll</button>
+		<div class="profContent">
+			<p>Email: cgcomadoll@mnu.edu</p>
+			<p>Office Number: 913-971-3667</p>
+			<p>Office: OS-226</p>
+			<p>Education: (PhD) Chemistry, 2021, University of Kansas, (BS) Chemistry, 2017, Appalachian State University</p>
+		</div>
+
+		<button type="button" class="profButton">Dr. Chris Crawford</button>
+		<div class="profContent">
+			<p>Email: cpcrawford@mnu.edu</p>
+			<p>Office Number: 913-971-3694</p>
+			<p>Office: OS-207</p>
+			<p>Education: (EdD) Kinesiology Education, 2020, University of North Carolina (MSEd) Biomechanics and Wellness, 2003, University of Kansas, (BS) Athletic Training, 2000, MidAmerica Nazarene University, Professional Teaching License, 2011, Baker University</p>
+		</div>
+		
+		<button type="button" class="profButton">Prof. Teresa Hale-Lespier</button>
+		<div class="profContent">
+			<p>Email: thalelespier@mnu.edu</p>
+			<p>Office Number: 913-971-3444</p>
+			<p>Office: OS-225</p>
+			<p>Education: (MS) Computer Science, 1999, Michigan Technical University, (BS) Computational Mathematics, 1991, Michigan Technical University</p>
+		</div>
+		
+		<button type="button" class="profButton">Dr. Abby Hodges</button>
+		<div class="profContent">
+			<p>Email: amhodges@mnu.edu</p>
+			<p>Office Number: 913-971-3671</p>
+			<p>Office: Metz-200J</p>
+			<p>Education: (PhD) 2008, Yale University, (MS) 2004, Yale University, (BS) 2002, Denison University</p>
+		</div>
+		
+		<button type="button" class="profButton">Prof. Brad King</button>
+		<div class="profContent">
+			<p>Email: bking@mnu.edu</p>
+			<p>Office Number: 913-971-3757</p>
+			<p>Office: OS-206</p>
+			<p>Education: (MEd) Physical Education/Sports Administration, 1986, Wichita State University, (BA) Physical Education, 1979, MidAmerica Nazarene University</p>
+		</div>
+		
+		<button type="button" class="profButton">Prof. Leigh Kreshel</button>
+		<div class="profContent">
+			<p>Email: lakreshel@mnu.edu</p>
+			<p>Office Number: 913-971-3668</p>
+			<p>Office: OS-205</p>
+			<p>Education: (MS) Exercise Science and Wellness, Wake Forest University, (BS) Health and Exercise Science, Wayne State College</p>
+		</div>
+		
+		<button type="button" class="profButton">Prof. James Leininger</button>
+		<div class="profContent">
+			<p>Email: jcleininger@mnu.edu</p>
+			<p>Office Number: 913-971-3666</p>
+			<p>Office: OS-224</p>
+			<p>Education: (MS) Mathematics, 1988, South Dakota State University, (BA) Houghton College</p>
+		</div>
+		
+		<button type="button" class="profButton">Dr. Jordan Mantha</button>
+		<div class="profContent">
+			<p>Email: jhmantha@mnu.edu</p>
+			<p>Office Number: 913-971-3676</p>
+			<p>Office: ML-126</p>
+			<p>Education: (PhD) Chemistry, 2009, University of Nevada, Reno, (BA) Environmental Science, 2002, University of Montana, Western</p>
+		</div>
+		
+		<button type="button" class="profButton">Dr. Craig Masters</button>
+		<div class="profContent">
+			<p>Email: cmasters@mnu.edu</p>
+			<p>Office Number: 913-971-3673</p>
+			<p>Office: OS-222</p>
+			<p>Education: (PhD) Physics/Mathematics, 2007, University of Missouri-Kansas City, (MS) Physics, 2005, University of Missouri-Kansas City, (BA)Physics, 2001, William Jewell College-Liberty, MO</p>
+		</div>
+		
+		<button type="button" class="profButton">Dr. Jill Speicher</button>
+		<div class="profContent">
+			<p>Email: jrspeicher@mnu.edu</p>
+			<p>Office Number: 913-971-3679</p>
+			<p>Office: OS-229</p>
+			<p>Education: (DVM) Veterinary Medicine, 2000, Iowa State University</p>
+		</div>
+		
+		<button type="button" class="profButton">Dr. Rion Taylor</button>
+		<div class="profContent">
+			<p>Email: rgtaylor@mnu.edu</p>
+			<p>Office Number: 913-971-3678</p>
+			<p>Office: OS-227</p>
+			<p>Education: (PhD) Biology, 2008, Syracuse University, (BS) Biology, 2000, Augusta State University</p>
+		</div>
+		
+		<div class="footer">
+			<img src="../images/MNU-full-logo.png" alt="MNU Full Logo" width="25%">
+		</div>
+	</div>
+	<h2>Take a look at our professor's schedules!</h2>
+	
+
+<script>
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+	function openNav() {
+  		document.getElementById("mySidenav").style.width = "20%";
+		document.getElementById("main").style.marginLeft = "20%";
+	}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+	function closeNav() {
+  		document.getElementById("mySidenav").style.width = "0";
+		document.getElementById("main").style.marginLeft = "0";
+	}
+	
+	var profButton = document.getElementsByClassName("profButton");
+	var x;
+	
+	for (x = 0; x < profButton.length; x++) {
+		profButton[x].addEventListener("click", function() {
+			this.classList.toggle("active");
+			var profContent = this.nextElementSibling;
+			if (profContent.style.display === "block") {
+				profContent.style.display = "none";
+			} else {
+				profContent.style.display = "block";
+			}
+		});
+	}
+</script>
+<?php/* broken dreams and lost hopes
+GRANT ALL ON *.* to username@localhost IDENTIFIED BY 'password';
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+if ($conn->query(CREATE DATABASE prof_schedules) === TRUE) {
+	echo "Database created successfully";
+} else {
+	echo "Error creating database: " . $conn->error;
+}
+
+if ($conn->query(CREATE TABLE course_info (
+ID INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+instructor VARCHAR(20) NOT NULL
+)) === TRUE) {
+	echo "Table course_info created successfully";
+} else {
+	echo "Error creating table course_info: " . $conn->error;
+}
+
+$conn->close;*/
+?>
+</body>
+</html>
